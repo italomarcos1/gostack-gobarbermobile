9f798733112cd437f72db29d9fd6019ebdb6594a
Italo M <italomarcos0010@gmail.com>
2020-06-03 21:31:12 -0300

    densidade de pixel

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, Image } from 'react-native';
 
 import { Container } from './styles';
+
+import logoImg from '../../assets/logo.png';
 
 const SignIn: React.FC = () => {
   return (
     <Container>
+      <Image source={logoImg} />
       <Text>SignIn</Text>
     </Container>
   );
 };
+
 export default SignIn;

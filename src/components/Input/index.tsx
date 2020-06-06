a0cbabcf61cef68848913b0751d00a2609967612
Italo M <italomarcos0010@gmail.com>
2020-06-06 15:25:48 -0300

    foco e preenchimento

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,7 +1,9 @@
 import React, {
   forwardRef,
+  useCallback,
   useEffect,
   useRef,
+  useState,
   useImperativeHandle,
 } from 'react';
 import { TextInputProperties } from 'react-native';
@@ -33,6 +35,19 @@ const Input: React.RefForwardingComponent<InputRef, InputProps> = (
   const { registerField, defaultValue = '', fieldName, error } = useField(name);
   const inputValueRef = useRef<InputValueReference>({ value: '' });
 
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+
+    setIsFilled(!!inputValueRef.current.value);
+  }, []);
+
   useImperativeHandle(ref, () => ({
     focus() {
       inputElementRef.current.focus();
@@ -56,13 +71,19 @@ const Input: React.RefForwardingComponent<InputRef, InputProps> = (
   }, [fieldName, registerField]);
 
   return (
-    <Container>
-      <Icon name={icon} size={20} color="#666360" />
+    <Container isFocused={isFocused}>
+      <Icon
+        name={icon}
+        size={20}
+        color={isFocused || isFilled ? '#ff9000' : '#666360'}
+      />
       <TextInput
         ref={inputElementRef}
         defaultValue={defaultValue}
         keyboardAppearance="dark"
         placeholderTextColor="#666360"
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
         {...rest}
         onChangeText={value => {
           inputValueRef.current.value = value;
